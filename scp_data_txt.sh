3184f568a5c300dfb86a7efebb2455802bcca3ca
YaXing Guo <guoyaxing@bosc.ac.cn>
2024-04-26 17:43:33 +0800

    Delete pack and scp_data_txt.sh
    Signed-off-by: YaXing Guo <guoyaxing@bosc.ac.cn>

--- scp_data_txt.sh
+++
@@ -1,7 +0,0 @@
-#scp ./data.txt guoyaxing@172.28.9.101:/nfs/home/guoyaxing/ns-bbl-build/uefi.txt
-#scp ./data.txt bosc@172.28.103.73:~/mytools/sw/uefi-sd-test/uefi.txt
-#scp ./data.txt guoyaxing@172.28.11.118:/nfs/home/guoyaxing/data.txt
-#scp ./data.txt guoyaxing@172.28.2.249:/nfs/home/share/guoyaxing/data.txt
-#scp ./build/gos.elf guoyaxing@172.28.2.249:/home/guoyaxing/gos.elf
-#scp ./entry/start.S guoyaxing@172.28.2.249:/home/guoyaxing/gos/entry/start.S
-scp ./data.txt guoyaxing@172.28.2.249:/nfs/home/share/guoyaxing/
