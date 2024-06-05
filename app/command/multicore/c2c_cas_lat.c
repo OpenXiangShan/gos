d2701cbef2be9299fa0bba3e1b0691c70bc4419f
YunFeng Yang <yangyunfeng@bosc.ac.cn>
2024-06-05 03:27:40 +0000

    c2cas: Fixing the stack overflow bug and formatting the code
    In previous versions, the size of the array duration was set to 10, but setting the variable k < 16 in the for loop caused a stack overflow problem. The nr_batch variable is now set to unify these values.
    
    Set MAX_CPU 16.
    
    Signed-off-by: YunFeng Yang <yangyunfeng@bosc.ac.cn>

--- app/command/multicore/c2c_cas_lat.c
+++ app/command/multicore/c2c_cas_lat.c
@@ -11,11 +11,12 @@
 #define CLOCK_FREQ 10000000
 #define US_PERCYCLE (1000000 / CLOCK_FREQ)
 
-#define MAX_CPU 32
+#define MAX_CPU 16
 
 static int task_args[4];
 static int condition = 0;
 static int nr_cpu = 2;
+static int nr_batch = 32;
 static int nr_repeat = 100000;
 static int PING = 0;
 static int PONG = 1;
@@ -23,126 +24,143 @@ static int duration_cpus[MAX_CPU];
 
 static int sem = 0;
 
-void sem_post() { __atomic_fetch_add(&sem, 1, __ATOMIC_SEQ_CST); }
-
-void sem_wait() {
-  while (__atomic_load_n(&sem, __ATOMIC_SEQ_CST) == 0)
-    ;
-  __atomic_fetch_sub(&sem, 1, __ATOMIC_SEQ_CST);
+void sem_post()
+{
+	__atomic_fetch_add(&sem, 1, __ATOMIC_SEQ_CST);
 }
 
-static int do_core2core_cas(void *data) {
-  int *ptr = (int *)data;
-  int thread_id = ptr[0];
-  int flag = ptr[1];
-
-  int i, ret, expected, disired;
-  int start, end;
-  if (flag) {
-    expected = PING;
-    disired = PONG;
-  } else {
-    expected = PONG;
-    disired = PING;
-  }
-
-  int expected_origin = expected;
-
-  start = get_system_tick();
-
-  for (i = 0; i < nr_repeat; i++) {
-  repeat:
-    ret = __atomic_compare_exchange_n(&condition, &expected, disired, 0,
-                                      __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
-    if (ret == 0) {
-      expected = expected_origin;
-      goto repeat;
-    }
-  }
-
-  end = get_system_tick();
-  duration_cpus[thread_id] = (end - start);
-  sem_post();
-  return 0;
+void sem_wait()
+{
+	while (__atomic_load_n(&sem, __ATOMIC_SEQ_CST) == 0) ;
+	__atomic_fetch_sub(&sem, 1, __ATOMIC_SEQ_CST);
 }
 
-static int cmd_c2c_cas_lat_handler(int argc, char *argv[], void *priv) {
-  print(" Core To Core CAS Latency \n");
+static int do_core2core_cas(void *data)
+{
+	int *ptr = (int *)data;
+	int thread_id = ptr[0];
+	int flag = ptr[1];
 
-  char *names[] = {"cpu0", "cpu1", "cpu2", "cpu3",
-                   "cpu4", "cpu5", "cpu6", "cpu7"};
+	int i, ret, expected, disired;
+	int start, end;
+	if (flag) {
+		expected = PING;
+		disired = PONG;
+	} else {
+		expected = PONG;
+		disired = PING;
+	}
 
-  int i, j, k;
+	int expected_origin = expected;
 
-  if (argc > 0) {
-    nr_cpu = atoi(argv[0]);
-  }
-  if (argc > 1) {
-    nr_repeat = atoi(argv[1]);
-  }
+	start = get_system_tick();
 
-  print("\n");
+	for (i = 0; i < nr_repeat; i++) {
+repeat:
+		ret =
+		    __atomic_compare_exchange_n(&condition, &expected, disired,
+						0, __ATOMIC_SEQ_CST,
+						__ATOMIC_SEQ_CST);
+		if (ret == 0) {
+			expected = expected_origin;
+			goto repeat;
+		}
+	}
 
-  task_args[0] = 0;
-  task_args[1] = 0;
-  task_args[2] = 0;
-  task_args[3] = 1;
+	end = get_system_tick();
+	duration_cpus[thread_id] = (end - start);
+	sem_post();
+	return 0;
+}
 
-  print("         ");
-  for(i=0; i < nr_cpu; i++) {
-    print("%d%s", i, "                   ");
-  }
-  print("\n");
+static int cmd_c2c_cas_lat_handler(int argc, char *argv[], void *priv)
+{
+	print(" Core To Core CAS Latency \n");
 
-  for (i = 0; i < nr_cpu; i++) {
-    print("%d ", i);
-    for (j = 0; j < nr_cpu; j++) {
-      if (i <= j) {
-        continue;
-      }
-      task_args[0] = i;
-      task_args[2] = j;
-      
-      int duration[10];
-      int dura_diff = 0;
-      int dura_ava;
-      int dura_tot = 0;
-      for (k = 0; k < 16; k++) {
-        create_task(names[i], do_core2core_cas, &task_args[0], i, NULL, 0, NULL);
-        create_task(names[j], do_core2core_cas, &task_args[2], j, NULL, 0, NULL);
-        
-        sem_wait();
-        sem_wait();
+	char *names[] = { "cpu0", "cpu1", "cpu2", "cpu3",
+		"cpu4", "cpu5", "cpu6", "cpu7",
+		"cpu8", "cpu9", "cpu10", "cpu11",
+		"cpu12", "cpu13", "cpu14", "cpu15"
+	};
 
-        duration[k] = (duration_cpus[i] + duration_cpus[j]) / 2;
-        dura_tot += duration[k];
-      }
+	int i, j, k;
 
-      dura_ava = dura_tot / 10;
+	if (argc > 0) {
+		nr_cpu = atoi(argv[0]);
+	}
+	if (argc > 1) {
+		nr_repeat = atoi(argv[1]);
+	}
 
-      for(k=0;k<10;k++) {
-        int diff = duration[k] > dura_ava ? duration[k] - dura_ava : dura_ava - duration[k];
-        dura_diff = dura_diff > diff ? dura_diff : diff;
-      }
-      
-      print("%7d(+-%7d) - ", dura_ava, dura_diff);
-    }
-    print("\n");
-  }
+	print("\n");
 
-  return 0;
+	task_args[0] = 0;
+	task_args[1] = 0;
+	task_args[2] = 0;
+	task_args[3] = 1;
+
+	print("         ");
+	for (i = 0; i < nr_cpu; i++) {
+		print("%d%s", i, "                   ");
+	}
+	print("\n");
+
+	for (i = 0; i < nr_cpu; i++) {
+		print("%d ", i);
+		for (j = 0; j < nr_cpu; j++) {
+			if (i <= j) {
+				continue;
+			}
+			task_args[0] = i;
+			task_args[2] = j;
+
+			int duration[nr_batch];
+			int dura_diff = 0;
+			int dura_ava;
+			int dura_tot = 0;
+			for (k = 0; k < nr_batch; k++) {
+				create_task(names[i], do_core2core_cas,
+					    &task_args[0], i, NULL, 0, NULL);
+				create_task(names[j], do_core2core_cas,
+					    &task_args[2], j, NULL, 0, NULL);
+
+				sem_wait();
+				sem_wait();
+
+				duration[k] =
+				    (duration_cpus[i] + duration_cpus[j]) / 2;
+				dura_tot += duration[k];
+			}
+
+			dura_ava = dura_tot / nr_batch;
+
+			for (k = 0; k < nr_batch; k++) {
+				int diff =
+				    duration[k] >
+				    dura_ava ? duration[k] -
+				    dura_ava : dura_ava - duration[k];
+				dura_diff = dura_diff > diff ? dura_diff : diff;
+			}
+
+			print("%7d(+-%7d) - ", dura_ava, dura_diff);
+		}
+		print("\n");
+	}
+
+	return 0;
 }
 
 static const struct command cmd_c2c_cas_lat = {
-    .cmd = "c2cas",
-    .handler = cmd_c2c_cas_lat_handler,
-    .priv = NULL,
+	.cmd = "c2cas",
+	.handler = cmd_c2c_cas_lat_handler,
+	.priv = NULL,
 };
 
-int cmd_c2c_cas_lat_init() {
-  register_command(&cmd_c2c_cas_lat);
+int cmd_c2c_cas_lat_init()
+{
+	register_command(&cmd_c2c_cas_lat);
 
-  return 0;
+	return 0;
 }
 
 APP_COMMAND_REGISTER(c2c_cas_lat, cmd_c2c_cas_lat_init);
--- app/command/multicore/cycas.c
+++ app/command/multicore/cycas.c
@@ -22,73 +22,76 @@ static int nr_repeat = 100000;
 
 static spinlock_t _lock;
 
-static int do_cyclic_cas(void *data) 
+static int do_cyclic_cas(void *data)
 {
-  int thread_id = *((int *)data);
-  print("Thread %d started\n", thread_id);
+	int thread_id = *((int *)data);
+	print("Thread %d started\n", thread_id);
 
-  int i, ret;
-  int expected = thread_id;
-  int expected_origin = thread_id;
-  int disired = (thread_id + 1) % nr_cpu;
-  int start = get_system_tick();
+	int i, ret;
+	int expected = thread_id;
+	int expected_origin = thread_id;
+	int disired = (thread_id + 1) % nr_cpu;
+	int start = get_system_tick();
 
-  for (i = 0; i < nr_repeat; i++) {
-  repeat:
-    ret = __atomic_compare_exchange_n(&condition, &expected, disired, 0,
-                                      __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
-    if (ret == 0) {
-      expected = expected_origin;
-      goto repeat;
-    } else {
-      cnt[thread_id]++;
-    }
-  }
+	for (i = 0; i < nr_repeat; i++) {
+repeat:
+		ret =
+		    __atomic_compare_exchange_n(&condition, &expected, disired,
+						0, __ATOMIC_SEQ_CST,
+						__ATOMIC_SEQ_CST);
+		if (ret == 0) {
+			expected = expected_origin;
+			goto repeat;
+		} else {
+			cnt[thread_id]++;
+		}
+	}
 
-  spin_lock(&_lock);
-  print("Thread %d Cnt: %d Ticks: %d \n", thread_id, cnt[thread_id],
-        get_system_tick() - start);
-  spin_unlock(&_lock);
-  return 0;
+	spin_lock(&_lock);
+	print("Thread %d Cnt: %d Ticks: %d \n", thread_id, cnt[thread_id],
+	      get_system_tick() - start);
+	spin_unlock(&_lock);
+	return 0;
 }
 
-static int cmd_cyclic_cas_handler(int argc, char *argv[], void *priv) 
+static int cmd_cyclic_cas_handler(int argc, char *argv[], void *priv)
 {
-  print(" Cyclic CAS TEST \n");
+	print(" Cyclic CAS TEST \n");
 
-  char *names[] = {"cpu0", "cpu1", "cpu2", "cpu3",
-                   "cpu4", "cpu5", "cpu6", "cpu7"};
+	char *names[] = { "cpu0", "cpu1", "cpu2", "cpu3",
+		"cpu4", "cpu5", "cpu6", "cpu7"
+	};
 
-  int i;
+	int i;
 
-if (argc > 0) {
-    nr_cpu = atoi(argv[0]);
-}
-if (argc > 1) {
-    nr_repeat = atoi(argv[1]);
-}
+	if (argc > 0) {
+		nr_cpu = atoi(argv[0]);
+	}
+	if (argc > 1) {
+		nr_repeat = atoi(argv[1]);
+	}
 
-  for (i = 0; i < nr_cpu; i++) 
-  {
-    cnt[i] = 0;
-    task_args[i] = i;
-    create_task(names[i], do_cyclic_cas, &task_args[i], i, NULL, 0, NULL);
-  }
+	for (i = 0; i < nr_cpu; i++) {
+		cnt[i] = 0;
+		task_args[i] = i;
+		create_task(names[i], do_cyclic_cas, &task_args[i], i, NULL, 0,
+			    NULL);
+	}
 
-  return 0;
+	return 0;
 }
 
 static const struct command cmd_cyclic_cas = {
-    .cmd = "cycas",
-    .handler = cmd_cyclic_cas_handler,
-    .priv = NULL,
+	.cmd = "cycas",
+	.handler = cmd_cyclic_cas_handler,
+	.priv = NULL,
 };
 
-int cmd_cyclic_cas_init() 
+int cmd_cyclic_cas_init()
 {
-  register_command(&cmd_cyclic_cas);
+	register_command(&cmd_cyclic_cas);
 
-  return 0;
+	return 0;
 }
 
 APP_COMMAND_REGISTER(cyclic_cas, cmd_cyclic_cas_init);
