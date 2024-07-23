d8861aca7d5389e2fc32b0753453ae7120a9b314
YaXing Guo <guoyaxing@bosc.ac.cn>
2024-07-23 14:08:02 +0800

    Remove some unnecessary logs in sleep.c
    Signed-off-by: YaXing Guo <guoyaxing@bosc.ac.cn>

--- myGuest/core/sleep.c
+++ myGuest/core/sleep.c
@@ -21,10 +21,7 @@ int schedule(void)
 int scheduler_early_init(struct device_init_entry *entry)
 {
 	struct device_init_entry *device_entry = entry;
-	print("%s %d\n", __FUNCTION__, __LINE__);
 	while (strncmp(device_entry->compatible, "THE END", sizeof("THE END"))) {
-		print("%s %d %s\n", __FUNCTION__, __LINE__,
-		      device_entry->compatible);
 		if (!strncmp(device_entry->compatible, "scheduler", 128)) {
 			scheduler_init(device_entry->start, device_entry->len,
 				       NULL);
