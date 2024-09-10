b694bdb880f2c0cfaf7767e778d9cd63956b606b
YaXing Guo <guoyaxing@bosc.ac.cn>
2024-09-10 18:20:22 +0800

    Support Zicond
    Signed-off-by: YaXing Guo <guoyaxing@bosc.ac.cn>

---
+++ include/asm/condition.h
@@ -0,0 +1,7 @@
+#ifndef __CONDITOINAL_H__
+#define __CONDITOINAL_H__
+
+unsigned long int czero_eqz(unsigned long val, unsigned long cond);
+unsigned long int czero_nqz(unsigned long val, unsigned long cond);
+
+#endif
