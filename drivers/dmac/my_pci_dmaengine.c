c546d1cf0c8642995fde068d7d30e2219e004b0a
YaXing Guo <guoyaxing@bosc.ac.cn>
2024-09-18 15:38:51 +0800

    Add my_pci_dmaengine driver
    Signed-off-by: YaXing Guo <guoyaxing@bosc.ac.cn>

---
+++ drivers/dmac/my_pci_dmaengine.c
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2024 Beijing Institute of Open Source Chip (BOSC)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 or later, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "my_pci_dmaengine.h"
+#include "print.h"
+#include "pci_device_driver.h"
+#include "pci.h"
+#include "vmap.h"
+#include "asm/type.h"
+#include "asm/mmio.h"
+#include "dmac.h"
+#include "mm.h"
+#include "cache_flush.h"
+#include "asm/barrier.h"
+#include "string.h"
+
+static unsigned long base;
+
+static void my_dmaengine_wait_for_complete(void)
+{
+	while (readl(base + MY_DMAENGINE_MMIO_CH0_DONE)) ;
+}
+
+static int my_dmaengine_ioctl(struct device *dev, unsigned int cmd, void *arg)
+{
+	int ret = 0;
+	struct dmac_ioctl_data *data = (struct dmac_ioctl_data *)arg;
+	unsigned long src_addr, des_addr;
+	unsigned int size;
+
+	switch (cmd) {
+	case MEM_TO_MEM:
+		src_addr = (unsigned long)data->src;
+		des_addr = (unsigned long)data->dst;
+		size = data->size;
+		writeq(base + MY_DMAENGINE_MMIO_CH0_SRC, src_addr);
+		writeq(base + MY_DMAENGINE_MMIO_CH0_DST, des_addr);
+		writel(base + MY_DMAENGINE_MMIO_CH0_TRAN_SIZE, size);
+		writel(base + MY_DMAENGINE_MMIO_CH0_START, 1);
+		mb();
+
+		my_dmaengine_wait_for_complete();
+
+		break;
+	}
+
+	return ret;
+}
+
+static const struct driver_ops my_dmaengine_ops = {
+	.ioctl = my_dmaengine_ioctl,
+};
+
+static int my_pci_dmaengine_init(struct pci_device *pdev, void *data)
+{
+	struct driver *drv;
+	struct device *dev = &pdev->dev;
+	struct resource res;
+	int size;
+
+	print("%s -- 0:%x:%x:%x\n",
+		__FUNCTION__, pdev->bus->bus_number, PCI_SLOT(pdev->devfn),
+		PCI_FUNC(pdev->devfn), pdev->vendor, pdev->device);
+
+	pci_enable_resource(pdev, 1 << 0);
+
+	pci_set_master(pdev, 1);
+
+	pci_get_resource(pdev, 0, &res);
+
+	size = res.end - res.base + 1;
+	print("ioremap addr:0x%lx size : 0x%x\n", res.base, size);
+	base = (unsigned long)ioremap((void *)res.base, res.end - res.base + 1, NULL);
+
+	drv = dev->drv;
+	strcpy(dev->name, "DMAC0");
+	strcpy(drv->name, "DMAC0");
+	drv->ops = &my_dmaengine_ops;
+
+	return 0;
+}
+
+PCI_DRIVER_REGISTER(my_pci_dmaengine, my_pci_dmaengine_init, 0x1234, 0x1);
+
---
+++ drivers/dmac/my_pci_dmaengine.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2024 Beijing Institute of Open Source Chip (BOSC)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 or later, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __MY_PCI_DMAENGINE_H__
+#define __MY_PCI_DMAENGINE_H__
+
+#define MY_DMAENGINE_MMIO_CH0_SRC        0x0
+#define MY_DMAENGINE_MMIO_CH0_DST        0x8
+#define MY_DMAENGINE_MMIO_CH0_TRAN_SIZE  0x100
+#define MY_DMAENGINE_MMIO_CH0_START      0x200
+#define MY_DMAENGINE_MMIO_CH0_DONE       0x10
+
+#define MY_DMAENGINE_MMIO_CH1_SRC        0x1000
+#define MY_DMAENGINE_MMIO_CH1_DST        0x1008
+#define MY_DMAENGINE_MMIO_CH1_TRAN_SIZE  0x1100
+#define MY_DMAENGINE_MMIO_CH1_START      0x1200
+#define MY_DMAENGINE_MMIO_CH1_DONE       0x1010
+
+#endif
+
