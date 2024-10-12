9a768d3ba5579c20542573a5a2d683202f6fbf84
YaXing Guo <guoyaxing@bosc.ac.cn>
2024-10-12 15:27:58 +0800

    Add include/generic
    Signed-off-by: YaXing Guo <guoyaxing@bosc.ac.cn>

---
+++ include/generic/align.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2024 Beijing Institute of Open Source Chip (BOSC)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 or later, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ALIGN_H__
+#define __ALIGN_H__
+
+#define ALIGN_MASK(x, mask) (((x) + (mask)) & ~(mask))
+#define ALIGN(p, a) ALIGN_MASK(p, (typeof(p))(a) - 1)
+#define PTR_ALIGN(p, a) (typeof(p))ALIGN((unsigned long)p, a)
+#define ALIGN_SIZE(addr, size) (((addr) + size - 1) & (~((size) - 1)))
+#define ALIGN_SIZE_UP(addr, size) ((addr) & ~((size) - 1))
+
+
+#define RESIZE(v, up) ((v / up == 0) ?  (v) : ((v / up + 1)*up))
+
+#endif
---
+++ include/generic/container_of.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2024 Beijing Institute of Open Source Chip (BOSC)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 or later, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CONTAINER_OF
+#define CONTAINER_OF
+
+#define offsetof(TYPE, MEMBER)  __builtin_offsetof(TYPE, MEMBER)
+
+/**
+ * container_of - cast a member of a structure out to the containing structure
+ * @ptr:        the pointer to the member.
+ * @type:       the type of the container struct this is embedded in.
+ * @member:     the name of the member within the struct.
+ *
+ * WARNING: any const qualifier of @ptr is lost.
+ */
+#define container_of(ptr, type, member) ({                              \
+        void *__mptr = (void *)(ptr);                                   \
+        ((type *)(__mptr - offsetof(type, member))); })
+
+#endif
