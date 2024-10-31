82e08714d169aa9fdf7e4bd81f8c9fffa421afdb
YaXing Guo <guoyaxing@bosc.ac.cn>
2024-10-31 10:28:23 +0800

    Command: stub
    Usage:
    stub xxx     -- register a stub on function xxx
    stub del     -- delete all stubs set using command stub
    stub del xxx -- delete the stub on function xxx
    stub info    -- show all stubs info
    
    Signed-off-by: YaXing Guo <guoyaxing@bosc.ac.cn>

--- app/command/stub_test.c
+++ app/command/stub_test.c
@@ -20,9 +20,18 @@
 #include "../command.h"
 #include "asm/mmio.h"
 #include "asm/ptregs.h"
-#include "stub.h"
 #include "string.h"
 #include "task.h"
+#include "list.h"
+#include "mm.h"
+#include "stub.h"
+
+struct stub_info {
+	struct list_head list;
+	char name[64];
+};
+
+static LIST_HEAD(stub_history);
 
 static void handle_ebreak_stub_handler(struct pt_regs *regs)
 {
@@ -44,10 +53,81 @@ static void handle_task_scheduler_event_stub_handler(struct pt_regs *regs)
 	print("schedule task: %s\n", task->name);
 }
 
+static int stub_del(char *name)
+{
+	struct stub_info *si, *n;
+
+	if (name == NULL) {
+		list_for_each_entry_safe(si, n, &stub_history, list) {
+			unregister_stub(si->name);
+			list_del(&si->list);
+		}
+		return 0;
+	}
+
+	list_for_each_entry_safe(si, n, &stub_history, list) {
+		if (!strcmp(si->name, name)) {
+			unregister_stub(si->name);
+			list_del(&si->list);
+			return 0;
+		}
+	}
+
+	return 0;
+}
+
+static void show_stub_info(void)
+{
+	struct stub_info *s;
+	int n = 0;
+
+	list_for_each_entry(s, &stub_history, list) {
+		print("%d : %s\n", n++, s->name);
+	}
+}
+
+static int cmd_stub_handler(int argc, char *argv[], void *priv)
+{
+	struct stub_info *stub;
+	if (argc == 0) {
+		print("Please input the stub function name\n");
+		return -1;
+	}
+
+	if (!strncmp(argv[0], "del", sizeof("del"))) {
+		if (argc == 2)
+			return stub_del(argv[1]);
+		else if (argc == 1)
+			return stub_del(NULL);
+		else {
+			print("Invalid params!\n");
+			return -1;
+		}
+	}
+	else if (!strncmp(argv[0], "info", sizeof("info"))) {
+		show_stub_info();
+		return 0;
+	}
+
+	if (register_stub(argv[0], default_stub_handler)) {
+		print("insert a stub into %s fail\n", argv[0]);
+		return -1;
+	}
+
+	stub = (struct stub_info *)mm_alloc(sizeof(struct stub_info));
+	if (!stub) {
+		print("alloc stub_info fail\n");
+		return -1;
+	}
+	strcpy(stub->name, argv[0]);
+	list_add_tail(&stub->list, &stub_history);
+
+	return 0;
+}
+
 static int cmd_stub_test_handler(int argc, char *argv[], void *priv)
 {
 	if (argc == 0) {
-		print("Please input the stub function name\n");
 		return -1;
 	}
 
@@ -62,14 +142,15 @@ static int cmd_stub_test_handler(int argc, char *argv[], void *priv)
 		register_stub("task_scheduler_event_handler",
 			      handle_task_scheduler_event_stub_handler);
 	}
-	else {
-		if (register_stub(argv[0], default_stub_handler)) {
-			print("insert a stub into %s fail\n", argv[0]);
-		}
-	}
 
 	return 0;
 }
+
+static const struct command cmd_stub = {
+	.cmd = "stub",
+	.handler = cmd_stub_handler,
+	.priv = NULL,
+};
 
 static const struct command cmd_stub_test = {
 	.cmd = "stub_test",
@@ -80,6 +161,7 @@ static const struct command cmd_stub_test = {
 int cmd_stub_test_init()
 {
 	register_command(&cmd_stub_test);
+	register_command(&cmd_stub);
 
 	return 0;
 }
