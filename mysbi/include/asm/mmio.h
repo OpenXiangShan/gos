92ce74cd1b377080e140776284b47215118e59e6
YaXing Guo <guoyaxing@bosc.ac.cn>
2024-11-05 17:53:03 +0800

    Fix bug of mysbi/include/asm/mmio.h
    Signed-off-by: YaXing Guo <guoyaxing@bosc.ac.cn>

--- mysbi/include/asm/mmio.h
+++ mysbi/include/asm/mmio.h
@@ -20,15 +20,14 @@
 #define __arch_getl(a)			(*(volatile unsigned int *)((unsigned long)a))
 #define __arch_putl(a,v)		(*(volatile unsigned int *)((unsigned long)a) = (v))
 
-#define __arch_getb(a)			(*(volatile unsigned char *)(unsigned long)(a))
+#define __arch_getb(a)			(*(volatile unsigned char *)((unsigned long)a))
 #define __arch_putb(a,v)		(*(volatile unsigned char *)((unsigned long)a) = (v))
 
 #define __arch_getq(a)			(*(volatile unsigned long *)((unsigned long)a))
 #define __arch_putq(a,v)		(*(volatile unsigned long *)((unsigned long)a) = (v))
 
-#define dmb()		__asm__ __volatile__ ("" : : : "memory")
-#define __iormb()	dmb()
-#define __iowmb()	dmb()
+#define __iormb()	__asm__ __volatile__ ("fence i,r" : : : "memory")
+#define __iowmb()	__asm__ __volatile__ ("fence w,o" : : : "memory")
 
 #define readl(a)	({ unsigned int  __v = __arch_getl(a); __iormb(); __v; })
 #define writel(a,v)	({ unsigned int  __v = v; __iowmb(); __arch_putl(a, __v);})
