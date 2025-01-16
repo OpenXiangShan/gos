37c4a93aecb8a7f6767d91f03f4ccd625db02117
Yaxing Guo <guoyaxing@bosc.ac.cn>
2025-01-16 13:41:00 +0800

    Add test case: svnapot_test_ext
    Signed-off-by: Yaxing Guo <guoyaxing@bosc.ac.cn>

--- app/command/svnapot_test.c
+++ app/command/svnapot_test.c
@@ -14,15 +14,88 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <asm/type.h>
-#include <print.h>
-#include <device.h>
+#include "asm/type.h"
+#include "asm/result.h"
+#include "print.h"
+#include "device.h"
 #include "../command.h"
 #include "vmap.h"
 #include "asm/pgtable.h"
 #include "string.h"
 #include "mm.h"
 #include "asm/tlbflush.h"
+
+#define GOOD_RESULT "Excepted result"
+#define BAD_RESULT "Unexcepted result"
+
+static int cmd_svnapot_test_ext_handler(int argc, char *argv[], void *priv)
+{
+	void *addr_napot;
+	void *addr_common[64];
+	unsigned long *pte[64];
+	int size_napot = PAGE_64K_SIZE;
+	int pnum = PAGE_64K_SIZE / PAGE_SIZE, i;
+
+	addr_napot = (void *)vmem_alloc_huge(size_napot, PAGE_64K_SIZE, NULL);
+	if (!addr_napot) {
+		print("Out of memory!\n");
+		return -1;
+	}
+
+	for (i = 0; i < pnum; i++) {
+		char *addr = (char *)addr_napot + i * PAGE_SIZE;
+		strcpy(addr, GOOD_RESULT);
+		pte[i] = mmu_get_pte((unsigned long)addr);
+		print("va:0x%lx pte:0x%lx\n", addr, *pte[i]);
+	}
+
+	for (i = 0; i < pnum - 1; i++) {
+		char *addr;
+
+		addr_common[i] = mm_alloc(PAGE_SIZE);
+		if (!addr_common[i]) {
+			print("Out of memory!\n");
+			return -1;
+		}
+
+		addr = (char *)addr_common[i];
+		strcpy(addr, BAD_RESULT);
+	}
+
+	print("1. Load addr_napot to let it fill into tlb, %s\n", (char *)addr_napot);
+	print("2. Modify the last %d PTEs in napot\n", pnum - 1);
+	for (i = 0; i < pnum - 1; i++) {
+		pgprot_t pgprot = __pgprot(_PAGE_BASE | _PAGE_READ | _PAGE_WRITE | _PAGE_DIRTY);
+		unsigned long addr = (unsigned long)addr_common[i];
+		unsigned long pfn = virt_to_phy(addr) >> PAGE_SHIFT;
+		*pte[i + 1] = pfn_pte(pfn, pgprot);
+	}
+
+	mb();
+
+	print("3. Load the last %d page in napot\n", pnum - 1);
+	for (i = 0; i < pnum - 1; i++) {
+		char *addr = (char *)addr_napot + (i + 1) * PAGE_SIZE;
+		print("addr:0x%lx -- %s\n", addr, addr);
+		if (strcmp(addr, GOOD_RESULT))
+			TEST_FAIL;
+	}
+
+	print("4. Load the last %d page after sfence\n", pnum - 1);
+
+	local_flush_tlb_all();
+	for (i = 0; i < pnum - 1; i++) {
+		char *addr = (char *)addr_napot + (i + 1) * PAGE_SIZE;
+		print("addr:0x%lx -- %s\n", addr, addr);
+		if (strcmp(addr, BAD_RESULT))
+			TEST_FAIL;
+	}
+
+
+	TEST_PASS;
+
+	return 0;
+}
 
 static int cmd_svnapot_test_handler(int argc, char *argv[], void *priv)
 {
@@ -55,9 +128,16 @@ static const struct command cmd_svnapot_test = {
 	.priv = NULL,
 };
 
+static const struct command cmd_svnapot_test_ext = {
+	.cmd = "svnapot_test_ext",
+	.handler = cmd_svnapot_test_ext_handler,
+	.priv = NULL,
+};
+
 int svnapot_test_init()
 {
 	register_command(&cmd_svnapot_test);
+	register_command(&cmd_svnapot_test_ext);
 
 	return 0;
 }
